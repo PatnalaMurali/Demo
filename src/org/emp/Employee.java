7ddf7915bc147663d0409b0964375bae93551500
PatnalaMurali <patnalamurali80@gmail.com>
2022-07-21 16:00:16 +0530

    new code added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,60 @@
+package org.emp;
+
+public class Employee {
+	
+	public void empId() {
+		System.out.println("empId  no is 8798");
+		
+		
+	}
+	public void empName() {
+		System.out.println("empName is murali()");
+		
+		
+		
+	}
+	public void empDob() {
+		System.out.println("emp dob is 7-8-9000");
+	}
+	
+public void empPhone() {
+	System.out.println("emp phone is 87976878887766");
+	
+	
+}
+public void empEmail() {
+	System.out.println("patnalal muarali@gmail.com");
+}
+public void empAddress() {
+	System.out.println("gajuwaka");
+	
+	
+}
+public static void main(String[] args ){
+	Employee e = new Employee();
+	e.empId();
+	e.empEmail();
+	e.empPhone();
+	e.empAddress();
+	e.empDob();
+	e.empName();
+	
+	
+			
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+}
